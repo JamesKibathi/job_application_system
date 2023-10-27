313e05660f158c85ffff2e4f16d1b0cabfc264ef
James Njenga <knjenga94@gmail.com>
2023-10-27 16:08:14 +0300

    Add create User action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,22 @@
 class UsersController < ApplicationController
+    rescue_from ActiveRecord::RecordInvalid, with: :invalid_user_details 
 
     def index
         users = User.all
         render json: users
+    end
+
+    def create
+        user = User.create!(user_params)
+        render json: user, status: :created
+    end
+
+    private
+    def user_params
+        params.permit(:email,:username,:password,:password_confirmation)
+    end
+
+    def invalid_user_details invalid
+        render json:{errors: invalid.record.errors.full_messages}, status: :unprocessable_entity
     end
 end
