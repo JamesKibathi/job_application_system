fd0b1970a0f3ab05780a21b96209390103746b3f
James Njenga <knjenga94@gmail.com>
2023-10-27 17:28:05 +0300

    Create JobApplications controller

---
+++ app/controllers/job_applications_controller.rb
@@ -0,0 +1,11 @@
+class JobApplicationsController < ApplicationController
+
+      
+    def create
+    # Handle user job applications
+    end
+
+    def index
+      # List user's job applications
+    end
+end
