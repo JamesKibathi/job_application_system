e474b00c74fd00ca270974eb3ea2161d4385c163
James Njenga <knjenga94@gmail.com>
2023-10-27 16:16:13 +0300

    Add profiles route

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   resources :users
 
+  resources :profiles
+
 
 
   # Defines the root path route ("/")
