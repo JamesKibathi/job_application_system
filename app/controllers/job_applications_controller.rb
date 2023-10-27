da0d91b47866945c8ac0728bdf43c844e03fc0b9
James Njenga <knjenga94@gmail.com>
2023-10-28 01:05:42 +0300

    Modify Code on Job Applications controller

--- app/controllers/job_applications_controller.rb
+++ app/controllers/job_applications_controller.rb
@@ -1,6 +1,7 @@
 class JobApplicationsController < ApplicationController
 
-  before_action :authenticate_user 
+  before_action :authenticate_user
+  before_action :set_job
 
   def create
     # Handle user job applications
@@ -23,8 +24,36 @@ class JobApplicationsController < ApplicationController
 
 
   private
-  
+
   def job_application_params
     params.require(:job_application).permit(:job_id)
   end
+
+  def authenticate_user
+    unless user_authenticated?
+      render json: {error:'Unauthorized'}, status: :unauthorized
+
+    end
+  end
+
+  def user_authenticated?
+    token = request.headers['Authorization']&.split('')&.last
+    secret_key = ENV['JWT_SECRET_KEY']
+    return false unless token
+    begin
+      decoded = JWT.decode(token, secret_key, true, algorithm: 'HS256')
+      user_id = decoded[0]['user_id']
+      current_user = User_find_by(id: user_id)
+      # Check if the user exists and is valid
+      return !current_user.nil?
+    rescue JWT::DecodeError
+      return false
+    end
+  end
+
+
+  def set_job
+    @job = Job.find(params[:job_id])
+  end
+
 end
