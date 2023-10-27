5e26790e9df17a3c23eaf5ced110c83a9bad7f73
James Njenga <knjenga94@gmail.com>
2023-10-27 17:13:47 +0300

    Add Logic to create new job

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,8 +1,22 @@
 class JobsController < ApplicationController
-
+    rescue_from ActiveRecord::RecordInvalid , with: :job_not_created
 
     def index
         jobs = Job.all
         render json: jobs 
+    end
+
+    def create
+        job = Job.create!(job_params)
+        render json: job, status: :created
+    end
+
+    private
+    def job_params
+        params.permit(:title,:description)
+    end
+
+    def job_not_created
+         render json:{errors: invalid.record.errors.full_messages}, status: :unprocessable_entity
     end
 end
