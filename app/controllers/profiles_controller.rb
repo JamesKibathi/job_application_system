5ee6893c89f7756b2ab7a1ba3cbf36ed4d510104
James Njenga <knjenga94@gmail.com>
2023-10-27 16:32:08 +0300

    Add create Profile action

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,6 +1,27 @@
 class ProfilesController < ApplicationController
+    rescue_from ActiveRecord::RecordInvalid , with: :profile_not_created
+
     def index
         profiles = Profile.all
         render json: profiles
     end
+
+
+    def create
+        profile = Profile.create!(profile_params)
+        render json: profile, status: :created
+    end
+
+    
+    private
+    def profile_params
+        params.permit(:first_name,:last_name,:bio,:resume_link,:user_id)
+    end
+
+    def profile_not_created
+        render json: {"errors": ["could not create profile"] }, status: :unprocessable_entity
+    end
+
+
+  
 end
