a936917ab28522e7cf337170c6d2bf9ffae7f837
James Njenga <knjenga94@gmail.com>
2023-10-27 15:20:36 +0300

    Validate inputs on User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,9 @@
 class User < ApplicationRecord
     has_secure_password
-
     has_one :profile
     has_many :job_applications
     has_many :jobs, through: :job_applications
+
+    validates :email, presence: true, uniqueness: true
+    validates :username, presence:true, uniqueness:true
 end
